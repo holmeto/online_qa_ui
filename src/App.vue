16e73ceee6b7651542bf9311196626d6e512318a
hutao.0809 <hutao.0809@bytedance.com>
2021-02-21 15:56:31 +0800

    调试初始页面

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-row type="flex" justify="center">
+      <el-col :xs="24" :sm="22" :md="20" :lg="20" :xl="18">
+        <router-view/>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+            }
+        },
+        methods: {
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0px;
+    padding: 0px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// // The Vue build version to load with the `import` command
+// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+
+// Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
+import qs from 'qs'
+import axios from 'axios'
 
+Vue.use(elementUI)
+Vue.prototype.axios = axios
+Vue.prototype.qs = qs
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
